575feb9bd5ac7433acf5854a8a2f36be117bdb44
James <LimaEchoCharlie@outlook.com>
2020-03-27 15:20:06 +0000

    IOTEDGE-919 add COAP client (#9)

--- pkg/things/thing.go
+++ pkg/things/thing.go
@@ -20,34 +20,17 @@ import (
 	"bytes"
 	"crypto"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"strings"
-	"time"
-
-	"github.com/ForgeRock/iot-edge/internal/amurl"
-	"github.com/ForgeRock/iot-edge/internal/debug"
-	"gopkg.in/square/go-jose.v2"
-	"gopkg.in/square/go-jose.v2/cryptosigner"
-	"gopkg.in/square/go-jose.v2/jwt"
 )
 
 // All SDK debug information is written to this Logger. The logger is muted by default. To see the debug output assign
 // your own logger (or a new one) to this variable.
 var DebugLogger = log.New(ioutil.Discard, "", 0)
 
-const (
-	acceptAPIVersion          = "Accept-API-Version"
-	serverInfoEndpointVersion = "resource=1.1"
-	authNEndpointVersion      = "protocol=1.0,resource=2.1"
-	commandEndpointVersion    = "protocol=2.0,resource=1.0"
-	contentType               = "Content-Type"
-	applicationJson           = "application/json"
-	applicationJose           = "application/jose"
-
-	defaultTimeout = 5 * time.Second
+var (
+	errAuthRequest = errors.New("authentication request failed")
 )
 
 // AuthenticatePayload represents the outbound and inbound data during an authentication request
@@ -86,171 +69,6 @@ type Client interface {
 
 	// sendCommand sends a command request to the ForgeRock platform
 	sendCommand(signer crypto.Signer, tokenID string, payload commandRequestPayload) (reply string, err error)
-}
-
-// AMClient contains information for connecting directly to AM
-type AMClient struct {
-	http.Client
-	ServerInfoURL string
-	AuthURL       string
-	IoTURL        string
-	cookieName    string
-}
-
-// NewAMClient returns a new client for connecting directly to AM
-func NewAMClient(baseURL, realm string) *AMClient {
-	r := amurl.RealmFromString(realm)
-	return &AMClient{
-		Client:        http.Client{Timeout: defaultTimeout},
-		ServerInfoURL: fmt.Sprintf("%s/json/serverinfo/*", baseURL),
-		AuthURL:       fmt.Sprintf("%s/json/authenticate?realm=%s&authIndexType=service&authIndexValue=", baseURL, r.Query()),
-		IoTURL:        fmt.Sprintf("%s/json/%s/iot?_action=command", baseURL, r.Path()),
-	}
-}
-
-// Initialise the AM client
-func (c *AMClient) Initialise() (Client, error) {
-	info, err := c.getServerInfo()
-	if err != nil {
-		return c, err
-	}
-	c.cookieName = info.CookieName
-	return c, nil
-}
-
-// Authenticate with the AM authTree using the given payload
-// This is a single round trip
-func (c *AMClient) Authenticate(authTree string, payload AuthenticatePayload) (reply AuthenticatePayload, err error) {
-	requestBody, err := json.Marshal(payload)
-	if err != nil {
-		return reply, err
-	}
-	request, err := http.NewRequest(http.MethodPost, c.AuthURL+authTree, bytes.NewBuffer(requestBody))
-	if err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, nil))
-		return reply, err
-	}
-	request.Header.Add(acceptAPIVersion, authNEndpointVersion)
-	request.Header.Add(contentType, applicationJson)
-	response, err := c.Do(request)
-	if err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return reply, err
-	}
-	defer response.Body.Close()
-	responseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return reply, err
-	}
-	if response.StatusCode != http.StatusOK {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return reply, fmt.Errorf("authentication request failed")
-	}
-	if err = json.Unmarshal(responseBody, &reply); err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return reply, err
-	}
-	return reply, err
-}
-
-// serverInfo contains information gathered from a server information request to AM
-type serverInfo struct {
-	CookieName string `json:"cookieName"`
-}
-
-// getServerInfo makes a server information request to AM
-func (c *AMClient) getServerInfo() (info serverInfo, err error) {
-	url := c.ServerInfoURL + "?_fields=cookieName"
-	request, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, nil))
-		return info, err
-	}
-	request.Header.Add(acceptAPIVersion, serverInfoEndpointVersion)
-	request.Header.Add(contentType, applicationJson)
-	response, err := c.Do(request)
-	if err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return info, err
-	}
-	defer response.Body.Close()
-	responseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return info, err
-	}
-	if response.StatusCode != http.StatusOK {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return info, fmt.Errorf("server info request failed")
-	}
-	if err = json.Unmarshal(responseBody, &info); err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return info, err
-	}
-	return info, err
-}
-
-func (c *AMClient) sendCommand(signer crypto.Signer, tokenID string, payload commandRequestPayload) (string, error) {
-	requestBody, err := signedJWTBody(signer, c.IoTURL, commandEndpointVersion, tokenID, payload)
-	DebugLogger.Println("Signed command request body: ", requestBody)
-	if err != nil {
-		return "", err
-	}
-	request, err := http.NewRequest(http.MethodPost, c.IoTURL, strings.NewReader(requestBody))
-	if err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, nil))
-		return "", err
-	}
-	request.Header.Set(acceptAPIVersion, commandEndpointVersion)
-	request.Header.Set(contentType, applicationJose)
-	request.AddCookie(&http.Cookie{Name: c.cookieName, Value: tokenID})
-	response, err := c.Do(request)
-	if err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return "", err
-	}
-	defer response.Body.Close()
-	responseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return "", err
-	}
-	if response.StatusCode != http.StatusOK {
-		DebugLogger.Println(debug.DumpRoundTrip(request, response))
-		return "", fmt.Errorf("request for command %s failed", payload.Command)
-	}
-	return string(responseBody), err
-}
-
-func signedJWTBody(signer crypto.Signer, url, version, tokenID string, body interface{}) (string, error) {
-	opts := &jose.SignerOptions{}
-	opts.WithHeader("aud", url)
-	opts.WithHeader("api", version)
-	// Note: nonce can be 0 as long as we create a new session for each request. If we reuse the token we need
-	// to increment the nonce between requests
-	opts.WithHeader("nonce", 0)
-
-	// check that the signer is supported
-	alg, err := signatureAlgorithm(signer)
-	if err != nil {
-		return "", err
-	}
-
-	// create a jose.OpaqueSigner from the crypto.Signer
-	opaque := cryptosigner.Opaque(signer)
-
-	sig, err := jose.NewSigner(jose.SigningKey{Algorithm: alg, Key: opaque}, opts)
-	if err != nil {
-		return "", err
-	}
-	builder := jwt.Signed(sig).Claims(struct {
-		Csrf string `json:"csrf"`
-	}{Csrf: tokenID})
-	if body != nil {
-		builder = builder.Claims(body)
-	}
-	return builder.CompactSerialize()
 }
 
 // Thing represents an AM Thing identity
