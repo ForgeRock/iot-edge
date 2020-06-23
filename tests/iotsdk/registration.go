5cc3eb6d096b98e03a611500ed8a4e04384c349f
James <LimaEchoCharlie@outlook.com>
2020-06-23 17:41:39 +0100

    IOTEDGE-962 add service type to SDK (#52)

--- tests/iotsdk/registration.go
+++ tests/iotsdk/registration.go
@@ -23,12 +23,12 @@ import (
 	"gopkg.in/square/go-jose.v2"
 )
 
-// RegisterThingCert tests the dynamic registration of a device with a valid x509 certificate
-type RegisterThingCert struct {
+// RegisterDeviceCert tests the dynamic registration of a device with a valid x509 certificate
+type RegisterDeviceCert struct {
 	anvil.NopSetupCleanup
 }
 
-func (t *RegisterThingCert) Setup(state anvil.TestState) (data anvil.ThingData, ok bool) {
+func (t *RegisterDeviceCert) Setup(state anvil.TestState) (data anvil.ThingData, ok bool) {
 	var err error
 	data.Id.Name = anvil.RandomName()
 	data.Id.ThingKeys, data.Signer, err = anvil.GenerateConfirmationKey(jose.ES256)
@@ -50,7 +50,7 @@ func (t *RegisterThingCert) Setup(state anvil.TestState) (data anvil.ThingData, 
 	return data, true
 }
 
-func (t *RegisterThingCert) Run(state anvil.TestState, data anvil.ThingData) bool {
+func (t *RegisterDeviceCert) Run(state anvil.TestState, data anvil.ThingData) bool {
 	builder := state.Builder(jwtPopRegCertTree)
 	builder.AddHandler(things.AuthenticateHandler{ThingID: data.Id.Name, ConfirmationKeyID: data.Signer.KID, ConfirmationKey: data.Signer.Signer})
 	builder.AddHandler(
@@ -64,12 +64,12 @@ func (t *RegisterThingCert) Run(state anvil.TestState, data anvil.ThingData) boo
 	return true
 }
 
-// RegisterThingCertWithoutCert tries to dynamically register a device without a x509 certificate
-type RegisterThingWithoutCert struct {
+// RegisterDeviceWithoutCert tries to dynamically register a device without a x509 certificate
+type RegisterDeviceWithoutCert struct {
 	anvil.NopSetupCleanup
 }
 
-func (t *RegisterThingWithoutCert) Setup(state anvil.TestState) (data anvil.ThingData, ok bool) {
+func (t *RegisterDeviceWithoutCert) Setup(state anvil.TestState) (data anvil.ThingData, ok bool) {
 	var err error
 	data.Id.Name = anvil.RandomName()
 	data.Id.ThingKeys, data.Signer, err = anvil.GenerateConfirmationKey(jose.ES256)
@@ -82,7 +82,7 @@ func (t *RegisterThingWithoutCert) Setup(state anvil.TestState) (data anvil.Thin
 	return data, true
 }
 
-func (t *RegisterThingWithoutCert) Run(state anvil.TestState, data anvil.ThingData) bool {
+func (t *RegisterDeviceWithoutCert) Run(state anvil.TestState, data anvil.ThingData) bool {
 	builder := state.Builder(jwtPopRegCertTree)
 	builder.AddHandler(
 		things.AuthenticateHandler{ThingID: data.Id.Name, ConfirmationKeyID: data.Signer.KID, ConfirmationKey: data.Signer.Signer})
@@ -98,12 +98,12 @@ func (t *RegisterThingWithoutCert) Run(state anvil.TestState, data anvil.ThingDa
 	return true
 }
 
-// RegisterThingWithAttributes tests the dynamic registration of a device with custom sttributes
-type RegisterThingWithAttributes struct {
+// RegisterDeviceWithAttributes tests the dynamic registration of a device with custom sttributes
+type RegisterDeviceWithAttributes struct {
 	anvil.NopSetupCleanup
 }
 
-func (t *RegisterThingWithAttributes) Setup(state anvil.TestState) (data anvil.ThingData, ok bool) {
+func (t *RegisterDeviceWithAttributes) Setup(state anvil.TestState) (data anvil.ThingData, ok bool) {
 	var err error
 	data.Id.Name = anvil.RandomName()
 	data.Id.ThingKeys, data.Signer, err = anvil.GenerateConfirmationKey(jose.ES256)
@@ -121,11 +121,10 @@ func (t *RegisterThingWithAttributes) Setup(state anvil.TestState) (data anvil.T
 		return data, false
 	}
 	data.Certificates = []*x509.Certificate{certificate}
-	data.Id.ThingType = things.TypeDevice
 	return data, true
 }
 
-func (t *RegisterThingWithAttributes) Run(state anvil.TestState, data anvil.ThingData) bool {
+func (t *RegisterDeviceWithAttributes) Run(state anvil.TestState, data anvil.ThingData) bool {
 	// 'serialNumber' is mapped to 'employeeNumber' in the registration node
 	sdkAttribute := struct {
 		SerialNumber string `json:"serialNumber"`
@@ -154,6 +153,46 @@ func (t *RegisterThingWithAttributes) Run(state anvil.TestState, data anvil.Thin
 	}
 	if len(amAttribute.EmployeeNumber) == 0 || amAttribute.EmployeeNumber[0] != sdkAttribute.SerialNumber {
 		anvil.DebugLogger.Printf("Expected attribute value %s; got %s", sdkAttribute.SerialNumber, amAttribute)
+		return false
+	}
+	return true
+}
+
+// RegisterServiceCert tests the dynamic registration of a service with a valid x509 certificate
+type RegisterServiceCert struct {
+	anvil.NopSetupCleanup
+}
+
+func (t *RegisterServiceCert) Setup(state anvil.TestState) (data anvil.ThingData, ok bool) {
+	var err error
+	data.Id.Name = anvil.RandomName()
+	data.Id.ThingKeys, data.Signer, err = anvil.GenerateConfirmationKey(jose.ES256)
+	if err != nil {
+		anvil.DebugLogger.Println("failed to generate confirmation key", err)
+		return data, false
+	}
+	serverWebKey, err := anvil.CertVerificationKey()
+	if err != nil {
+		return data, false
+	}
+
+	certificate, err := anvil.CreateCertificate(serverWebKey, data.Id.Name, data.Signer.Signer)
+	if err != nil {
+		return data, false
+	}
+	data.Certificates = []*x509.Certificate{certificate}
+	return data, true
+}
+
+func (t *RegisterServiceCert) Run(state anvil.TestState, data anvil.ThingData) bool {
+	builder := state.Builder(jwtPopRegCertTree)
+	builder.AddHandler(things.AuthenticateHandler{ThingID: data.Id.Name, ConfirmationKeyID: data.Signer.KID, ConfirmationKey: data.Signer.Signer})
+	builder.AddHandler(
+		things.RegisterHandler{ThingID: data.Id.Name, ThingType: things.TypeService, ConfirmationKeyID: data.Signer.KID,
+			ConfirmationKey: data.Signer.Signer, Certificates: data.Certificates},
+	)
+	_, err := builder.Initialise()
+	if err != nil {
 		return false
 	}
 	return true
