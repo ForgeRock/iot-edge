108ab202f519ea9cf852a453d8f68ae90c4ebe85
Jaco Jooste <jaco.jooste@forgerock.com>
2020-10-23 16:22:25 +0100

    IOTEDGE-1068 Integrate platform with HiveMQ

---
+++ examples/proof-of-concept/hivemq/mqtt-client/go.mod
@@ -0,0 +1,9 @@
+module mqtt-client
+
+go 1.14
+
+require (
+	github.com/ForgeRock/iot-edge/v7 v7.0.1-0.20201005135449-4f6c39f16220
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	golang.org/x/net v0.0.0-20200930145003-4acb6c075d10 // indirect
+)
---
+++ examples/proof-of-concept/hivemq/mqtt-client/main.go
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2020 ForgeRock AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"github.com/ForgeRock/iot-edge/v7/examples/secrets"
+	"github.com/ForgeRock/iot-edge/v7/pkg/builder"
+	"github.com/ForgeRock/iot-edge/v7/pkg/thing"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	//thing.SetDebugLogger(log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile))
+
+	// ForgeRock connection information
+	thingID := "47cf707c-80c1-4816-b067-99db2a443113"
+	signer := secrets.Signer(thingID)
+	certificate := []*x509.Certificate{secrets.Certificate(thingID, signer.Public())}
+	keyID, _ := thing.JWKThumbprint(signer)
+	amURL, _ := url.Parse(os.Getenv("AM_URL"))
+
+	// MQTT connection information
+	// Can be retrieved from configuration
+	server := os.Getenv("MQTT_SERVER_URL")
+	qos := byte(2)
+	topic := "test"
+
+	dynamicThing, err := builder.Thing().
+		ConnectTo(amURL).
+		InRealm("/").
+		WithTree("RegisterThings").
+		AuthenticateThing(thingID, "/", keyID, signer, nil).
+		RegisterThing(certificate, nil).
+		Create()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(thingID, " registration successful")
+
+	connOpts := mqtt.NewClientOptions().
+		AddBroker(server).
+		SetCleanSession(true)
+
+	connOpts.SetCredentialsProvider(func() (username string, password string) {
+		tokenResponse, err := dynamicThing.RequestAccessToken("publish")
+		if err != nil {
+			log.Fatal(err)
+		}
+		password, err = tokenResponse.AccessToken()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Providing the OAuth 2.0 access token as password: %s", password)
+		return thingID, password
+	})
+
+	tlsConfig := &tls.Config{InsecureSkipVerify: true, ClientAuth: tls.NoClientCert}
+	connOpts.SetTLSConfig(tlsConfig)
+	connOpts.SetWill(topic, "Game Over", qos, false)
+
+	client := mqtt.NewClient(connOpts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+
+	fmt.Printf("Connected to %s\n", server)
+	msgCount := 10
+	for msgCount > 0 {
+		msg := "Time now is " + time.Now().Format(time.RFC3339)
+		if token := client.Publish(topic, qos, false, msg); token.Wait() && token.Error() != nil {
+			log.Fatal(token.Error())
+		}
+		log.Printf("message \"%s\" sent", msg)
+		msgCount--
+		time.Sleep(time.Second * 2)
+	}
+	client.Disconnect(250)
+}
