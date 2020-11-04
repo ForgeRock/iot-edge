b9a6695426b3baf7b042dccc9d33f20d38eb83fb
James <LimaEchoCharlie@outlook.com>
2020-11-04 18:02:32 +0000

    IOTEDGE-1071 update GCP IoT identities from FR Platform

--- examples/proof-of-concept/gcp-iot/gcp-iot-core-connector/src/main/java/org/forgerock/openicf/connectors/google/GCPIoTCoreConnector.java
+++ examples/proof-of-concept/gcp-iot/gcp-iot-core-connector/src/main/java/org/forgerock/openicf/connectors/google/GCPIoTCoreConnector.java
@@ -8,60 +8,34 @@
 
 package org.forgerock.openicf.connectors.google;
 
-import java.io.ByteArrayInputStream;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.cloudiot.v1.CloudIot;
+import com.google.api.services.cloudiot.v1.CloudIotScopes;
+import com.google.api.services.cloudiot.v1.model.Device;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.exceptions.ConnectorIOException;
+import org.identityconnectors.framework.common.exceptions.ConnectorSecurityException;
+import org.identityconnectors.framework.common.objects.*;
+import org.identityconnectors.framework.common.objects.filter.Filter;
+import org.identityconnectors.framework.common.objects.filter.FilterTranslator;
+import org.identityconnectors.framework.spi.*;
+import org.identityconnectors.framework.spi.operations.*;
+
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-
-import org.identityconnectors.common.logging.Log;
-import org.identityconnectors.framework.common.exceptions.ConnectorException;
-import org.identityconnectors.framework.common.exceptions.ConnectorIOException;
-import org.identityconnectors.framework.common.objects.Attribute;
-import org.identityconnectors.framework.common.objects.AttributeBuilder;
-import org.identityconnectors.framework.common.objects.AttributeInfo;
-import org.identityconnectors.framework.common.objects.AttributeInfoBuilder;
-import org.identityconnectors.framework.common.objects.ConnectorObject;
-import org.identityconnectors.framework.common.objects.ConnectorObjectBuilder;
-import org.identityconnectors.framework.common.objects.Name;
-import org.identityconnectors.framework.common.objects.ObjectClass;
-import org.identityconnectors.framework.common.objects.ObjectClassInfoBuilder;
-import org.identityconnectors.framework.common.objects.OperationOptions;
-import org.identityconnectors.framework.common.objects.ResultsHandler;
-import org.identityconnectors.framework.common.objects.Schema;
-import org.identityconnectors.framework.common.objects.SchemaBuilder;
-import org.identityconnectors.framework.common.objects.SearchResult;
-import org.identityconnectors.framework.common.objects.SyncDeltaBuilder;
-import org.identityconnectors.framework.common.objects.SyncDeltaType;
-import org.identityconnectors.framework.common.objects.SyncResultsHandler;
-import org.identityconnectors.framework.common.objects.SyncToken;
-import org.identityconnectors.framework.common.objects.Uid;
-import org.identityconnectors.framework.common.objects.filter.Filter;
-import org.identityconnectors.framework.common.objects.filter.FilterTranslator;
-import org.identityconnectors.framework.spi.Configuration;
-import org.identityconnectors.framework.spi.Connector;
-import org.identityconnectors.framework.spi.ConnectorClass;
-import org.identityconnectors.framework.spi.SearchResultsHandler;
-import org.identityconnectors.framework.spi.SyncTokenResultsHandler;
-import org.identityconnectors.framework.spi.operations.SchemaOp;
-import org.identityconnectors.framework.spi.operations.SearchOp;
-import org.identityconnectors.framework.spi.operations.SyncOp;
-import org.identityconnectors.framework.spi.operations.TestOp;
-
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.services.cloudiot.v1.CloudIot;
-import com.google.api.services.cloudiot.v1.CloudIotScopes;
-import com.google.api.services.cloudiot.v1.model.Device;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.http.HttpCredentialsAdapter;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import java.util.Set;
 
 /**
  * Main implementation of the Google Cloud Platform IoT Core Connector.
  */
 @ConnectorClass(displayNameKey = "GCPIoTCore.connector.display", configurationClass = GCPIoTCoreConfiguration.class)
-public class GCPIoTCoreConnector implements Connector, TestOp, SchemaOp, SearchOp<Filter>, SyncOp {
+public class GCPIoTCoreConnector implements Connector, TestOp, SchemaOp, SearchOp<Filter>, SyncOp, UpdateOp {
     private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS`Z`");
     private static final AttributeInfo THING_TYPE_ATTR_INFO = AttributeInfoBuilder.build("thingType", String.class);
     private static final AttributeInfo STATUS_ATTR_INFO = AttributeInfoBuilder.build("accountStatus", String.class);
@@ -73,16 +47,22 @@ public class GCPIoTCoreConnector implements Connector, TestOp, SchemaOp, SearchO
     private GCPIoTCoreConfiguration configuration;
     private Schema schema = null;
 
-    private CloudIot getService() throws IOException, GeneralSecurityException {
-        GoogleCredentials credentials = GoogleCredentials.fromStream(this.configuration.getCredentialsAsStream())
-                .createScoped(CloudIotScopes.all());
-        credentials.refreshIfExpired();
-        CloudIot.Builder builder = new CloudIot.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(),
-                new HttpCredentialsAdapter(credentials));
-        builder.setApplicationName("idm-connector");
-        return builder.build();
+    private CloudIot getService() throws ConnectorIOException, ConnectorSecurityException {
+        try {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(this.configuration.getCredentialsAsStream())
+                    .createScoped(CloudIotScopes.all());
+            credentials.refreshIfExpired();
+            CloudIot.Builder builder = new CloudIot.Builder(
+                    GoogleNetHttpTransport.newTrustedTransport(),
+                    JacksonFactory.getDefaultInstance(),
+                    new HttpCredentialsAdapter(credentials));
+            builder.setApplicationName("idm-connector");
+            return builder.build();
+        } catch (IOException e) {
+            throw new ConnectorIOException("Cloud IoT service", e);
+        } catch (GeneralSecurityException e) {
+            throw new ConnectorIOException("Cloud IoT service", e);
+        }
     }
 
     private String getRegistryPath() {
@@ -92,15 +72,19 @@ public class GCPIoTCoreConnector implements Connector, TestOp, SchemaOp, SearchO
                 this.configuration.getRegistryId());
     }
 
-    private List<Device> getDevices(CloudIot service) throws IOException {
-        return service.projects()
-                .locations()
-                .registries()
-                .devices()
-                .list(getRegistryPath())
-                .setFieldMask("blocked")
-                .execute()
-                .getDevices();
+    private List<Device> getDevices(CloudIot service) throws ConnectorIOException {
+        try {
+            return service.projects()
+                    .locations()
+                    .registries()
+                    .devices()
+                    .list(getRegistryPath())
+                    .setFieldMask("blocked")
+                    .execute()
+                    .getDevices();
+        } catch (IOException e) {
+           throw new ConnectorIOException("Device list failed", e);
+        }
     }
 
     @Override
@@ -129,9 +113,9 @@ public class GCPIoTCoreConnector implements Connector, TestOp, SchemaOp, SearchO
     public void sync(ObjectClass objectClass, SyncToken syncToken, SyncResultsHandler handler,
             OperationOptions operationOptions) {
         isThing(objectClass);
+        CloudIot service = getService();
+        List<Device> devices = getDevices(service);
         try {
-            CloudIot service = getService();
-            List<Device> devices = getDevices(service);
             for( Device d : devices ) {
                 SyncDeltaBuilder deltaBuilder = new SyncDeltaBuilder();
                 deltaBuilder.setObject(buildThing(d));
@@ -142,7 +126,7 @@ public class GCPIoTCoreConnector implements Connector, TestOp, SchemaOp, SearchO
                 }
             }
             ((SyncTokenResultsHandler) handler).handleResult(syncToken);
-        } catch (IOException | GeneralSecurityException e) {
+        } catch (IOException e) {
             logger.error("Device sync failed.", e);
             throw new ConnectorIOException(e);
         }
@@ -179,9 +163,9 @@ public class GCPIoTCoreConnector implements Connector, TestOp, SchemaOp, SearchO
     @Override
     public void executeQuery(ObjectClass objectClass, Filter query, ResultsHandler handler, OperationOptions options) {
         isThing(objectClass);
+        CloudIot service = getService();
+        List<Device> devices = getDevices(service);
         try {
-            CloudIot service = getService();
-            List<Device> devices = getDevices(service);
             if (devices == null) {
                 logger.error(String.format("empty list returned for %s", getRegistryPath()));
                 return;
@@ -193,24 +177,17 @@ public class GCPIoTCoreConnector implements Connector, TestOp, SchemaOp, SearchO
                 }
             }
             ((SearchResultsHandler) handler).handleResult(new SearchResult());
-        } catch (IOException | GeneralSecurityException e) {
+        } catch (IOException e) {
             logger.error("Device query failed.", e);
             throw new ConnectorIOException(e);
         }
     }
 
+
     @Override
     public void test() {
         // test the connection to the IoT Hub
-        try {
-            getService();
-        } catch (IOException e) {
-            logger.error("IoT Hub connection failed.", e);
-            throw new ConnectorIOException(e);
-        } catch (GeneralSecurityException e) {
-            logger.error("IoT Hub connection failed.", e);
-            throw new ConnectorIOException(e);
-        }
+        getService();
     }
 
     private ConnectorObject buildThing(Device device) throws IOException {
@@ -232,6 +209,35 @@ public class GCPIoTCoreConnector implements Connector, TestOp, SchemaOp, SearchO
         if (!THINGS.equals(objectClass)) {
             throw new IllegalArgumentException(String.format("Operation requires ObjectClass %s, received %s",
                     THINGS.getDisplayNameKey(), objectClass));
+        }
+    }
+
+    @Override
+    public Uid update(ObjectClass objectClass, Uid uid, Set<Attribute> set, OperationOptions operationOptions) {
+        isThing(objectClass);
+        CloudIot service = getService();
+        AttributesAccessor attributesAccessor = new AttributesAccessor(set);
+        if( attributesAccessor.hasAttribute(STATUS_ATTR_INFO.getName())) {
+            updateBlocked(service, uid, attributesAccessor.findString(STATUS_ATTR_INFO.getName()));
+        }
+        return uid;
+    }
+
+    private void updateBlocked(CloudIot service, Uid uid, String accountStatus) throws ConnectorIOException {
+        final String devicePath = String.format("%s/devices/%s", getRegistryPath(), uid.getUidValue());
+        logger.info("update blocked for {0} with account status {1}", devicePath, accountStatus);
+
+        Device thing = new Device().setBlocked(accountStatus.equals("inactive"));
+        try {
+            service.projects()
+                    .locations()
+                    .registries()
+                    .devices()
+                    .patch(devicePath, thing)
+                    .setUpdateMask("blocked")
+                    .execute();
+        } catch (IOException e) {
+            throw new ConnectorIOException("Device blocked update failed", e);
         }
     }
 }
