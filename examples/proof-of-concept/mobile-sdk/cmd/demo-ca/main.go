29ce645ebaebdeb1e3f2b20bdc3ba6ae23a738d0
limaechocharlie <LimaEchoCharlie@outlook.com>
2021-10-28 15:38:17 +0100

    Add error checking to dummy server

--- examples/proof-of-concept/mobile-sdk/cmd/demo-ca/main.go
+++ examples/proof-of-concept/mobile-sdk/cmd/demo-ca/main.go
@@ -30,17 +30,49 @@ import (
 	"time"
 )
 
+var key jose.JSONWebKey
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	data, _ := ioutil.ReadAll(r.Body)
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading body; %s", err), http.StatusBadRequest)
+		return
+	}
+	log.Printf("Received:\n%s\n", string(data))
+
 	b, _ := pem.Decode(data)
 	var csr *x509.CertificateRequest
-	var err error
 	if b == nil {
 		csr, err = x509.ParseCertificateRequest(data)
 	} else {
 		csr, err = x509.ParseCertificateRequest(b.Bytes)
 	}
-	fmt.Println("CSR", csr, err)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Unable body to parse into a x509 CSR; %s", err),
+			http.StatusBadRequest)
+		return
+	}
+	certBytes, err := x509.CreateCertificate(rand.Reader,
+		&x509.Certificate{
+			SerialNumber: big.NewInt(2),
+			Subject:      csr.Subject,
+			NotBefore:    time.Now().Add(-24 * time.Hour),
+			NotAfter:     time.Now().Add(365 * 24 * time.Hour),
+		},
+		key.Certificates[0],
+		csr.PublicKey,
+		key.Key)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Unable create certificate x509 certificate; %s", err),
+			http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Created certificate for %s\n", csr.Subject)
+	w.Write(certBytes)
+}
+
+func main() {
 	ec256TestBytes := []byte(`{"kty": "EC",
 		"kid": "Fol7IpdKeLZmzKtCEgi1LDhSIzM=",
 		"x": "N7MtObVf92FJTwYvY2ZvTVT3rgZp7a7XDtzT_9Rw7IA",
@@ -50,27 +82,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		"d": "w9rAMaNcP7cA0e5SECc4Tk1PDQEY66ml9y9-6E8fmR4",
 		"x5c": ["MIIBwjCCAWkCCQCw3GyPBTSiGzAJBgcqhkjOPQQBMGoxCzAJBgNVBAYTAlVLMRAwDgYDVQQIEwdCcmlzdG9sMRAwDgYDVQQHEwdCcmlzdG9sMRIwEAYDVQQKEwlGb3JnZVJvY2sxDzANBgNVBAsTBk9wZW5BTTESMBAGA1UEAxMJZXMyNTZ0ZXN0MB4XDTE3MDIwMzA5MzQ0NloXDTIwMTAzMDA5MzQ0NlowajELMAkGA1UEBhMCVUsxEDAOBgNVBAgTB0JyaXN0b2wxEDAOBgNVBAcTB0JyaXN0b2wxEjAQBgNVBAoTCUZvcmdlUm9jazEPMA0GA1UECxMGT3BlbkFNMRIwEAYDVQQDEwllczI1NnRlc3QwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAAQ3sy05tV/3YUlPBi9jZm9NVPeuBmntrtcO3NP/1HDsgLsTZsqKHD6KWIeJNRQnONcriWVaIcZYTKNykyCVUz93MAkGByqGSM49BAEDSAAwRQIgZhTox7WpCb9krZMyHfgCzHwfu0FVqaJsO2Nl2ArhCX0CIQC5GgWD5jjCRlIWSEFSDo4DZgoQFXaQkJUSUbJZYpi9dA=="]
 	}`)
-	var key jose.JSONWebKey
 	if err := json.Unmarshal(ec256TestBytes, &key); err != nil {
-		fmt.Errorf("KEY %v", err)
+		log.Fatal(err)
 	}
-	cert, err := x509.CreateCertificate(rand.Reader,
-		&x509.Certificate{
-			SerialNumber: big.NewInt(2),
-			Subject:      csr.Subject,
-			NotBefore:    time.Now().Add(-24 * time.Hour),
-			NotAfter:     time.Now().Add(365* 24 * time.Hour),
-		},
-		key.Certificates[0],
-		csr.PublicKey,
-		key.Key)
-	_, err = x509.ParseCertificate(cert)
-	fmt.Println("END", cert, err)
 
-	w.Write(cert)
-}
-
-func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8088", nil))
 }
